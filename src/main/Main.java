5407eb8622a11002ebba9b45a1124b3d0b22572f
JHS <uolk0600@naver.com>
2022-04-11 12:18:08 +0900

    팀장 코드

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		System.out.println("원격 저장소 사용");
 		System.out.println("원격 저장소 사용2");
 		System.out.println("팀장 코드 시작입니다.");
-		System.out.println("팀장 코드 마지막입니다.");
+		System.out.println("팀장 코드 마지막입니다..");
 
 	}
 
