7f118a6411aed3216f301e494fb063c80bdc8a78
JHS <uolk0600@naver.com>
2022-04-11 12:17:02 +0900

    팀장 커밋 완료

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,15 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-     
+
+		System.out.println("로컬 저장소 사용");
+		System.out.println("원격 저장소 사용");
+		System.out.println("원격 저장소 사용2");
+		System.out.println("팀장 코드 시작입니다.");
+      System.out.println("팀장 코드 마지막입니다.");
 		
 		
-		 System.out.println("로컬 저장소 사용");
-	   System.out.println("원격 저장소 사용");
-	   System.out.println("원격 저장소 사용2");
-	
+		
 	}
 
 }
