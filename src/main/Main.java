ad31fcf418acc2b817129430495314b10d345b85
jeonheesu <103397167+jeonheesu@users.noreply.github.com>
2022-04-11 12:21:17 +0900

    팀원이 14번 라인부터 16번 라인까지 코드완료

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,11 @@ public class Main {
 		System.out.println("원격 저장소 사용2");
 		System.out.println("팀장 코드 시작입니다.");
 		System.out.println("팀장 코드 마지막입니다..");
-
+		
+  
+                System.out.println("팀장 코드 1..");
+		System.out.println("팀장 코드 2..");
+		System.out.println("팀장 코드 3..");
 	}
 
 }
